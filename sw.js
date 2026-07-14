b3c98945d11085f92250357cc161fc9c3114b1d7
zy-2006-0223 <1250856112@qq.com>
2026-07-14 14:38:42 +0800

    Initial commit: daily work log platform

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'daily-work-log-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        if (res.type === 'basic' || res.type === 'cors') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => new Response('离线模式', { status: 200 }))
+    )
+  );
+});
